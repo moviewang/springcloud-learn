567dfcbe938cd7746fa8fb2f85d761989df9132a
movie <2314165649@qq.com>
2021-08-19 20:11:15 +0800

    feat:eureka with Spring security

---
+++ eureka-server/src/main/java/com/eurekaserver/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.eurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author: movie
+ * @Date: 2021/8/19 19:57
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
