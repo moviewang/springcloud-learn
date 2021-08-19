d1f855f7fd6023d82ae7722a5142889c832f0ae0
movie <2314165649@qq.com>
2021-08-19 19:20:33 +0800

    feat:spring-cloud-config with eureka

--- config-client/src/main/java/com/bytedance/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/bytedance/configclient/ConfigClientApplication.java
@@ -2,11 +2,13 @@ package com.bytedance.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: movie
  * @Date: 2021/8/18 19:15
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
     public static void main(String[] args) {
