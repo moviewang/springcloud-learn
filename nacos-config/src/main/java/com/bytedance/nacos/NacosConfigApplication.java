a5f1f4e9c296bee93a4a7c68e6eaa1a1d8bd8c34
movie <2314165649@qq.com>
2021-08-22 17:09:26 +0800

    feat:nacos config

---
+++ nacos-config/src/main/java/com/bytedance/nacos/NacosConfigApplication.java
@@ -0,0 +1,15 @@
+package com.bytedance.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: movie
+ * @Date: 2021/8/20 19:06
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+}
---
+++ nacos-config/src/main/java/com/bytedance/nacos/controller/ConfigController.java
@@ -0,0 +1,23 @@
+package com.bytedance.nacos.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: movie
+ * @Date: 2021/8/20 19:18
+ */
+@RestController
+@RequestMapping("config")
+@RefreshScope
+public class ConfigController {
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
