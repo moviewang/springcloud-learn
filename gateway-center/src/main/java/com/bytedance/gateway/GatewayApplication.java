7cada0afa40d3fb9602466e7d346c633f7920dbf
movie <2314165649@qq.com>
2021-08-31 17:00:00 +0800

    feat: add spring-cloud-gateway

---
+++ gateway-center/src/main/java/com/bytedance/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.bytedance.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: movie
+ * @Date: 2021/8/31 16:05
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
